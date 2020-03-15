bd1d6636866a4df3b08de38d8dbc47ac30ba73ec
Bitho <60443195+bitholina@users.noreply.github.com>
2020-03-15 17:21:37 +0100

    Answers Week 2 programming foundations

---
+++ script.js
@@ -0,0 +1,55 @@
+// question 1 
+
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+
+console.log(petArray[1]);
+
+// question 3
+
+petArray.push("sheep");
+
+// question 4
+
+console.log(petArray.length);
+
+// question 5
+
+var catObject = {
+    name: "Meow",
+    color: "gray",
+    age: 5,
+};
+
+// question 6
+
+console.log(catObject.age);
+
+// question 7 
+
+var catArray = [
+    {
+        name: "Meow", 
+        color: "gray", 
+        age: 5
+    }
+];
+
+// question 8
+
+for(var i = 0; i < catArray.length; i++) {
+    console.log(catArray[i]);
+}
+
+// question 9 
+
+ function logToConsole(animal){
+     console.log(animal);
+ }
+
+// question 10
+
+logToConsole("donkey");
+
+
